efb16ce968949a6b39b0316bd2c22a3a2999d20c
tibinsunny <tibinsunny95@gmail.com>
2022-01-22 21:32:27 +0530

    build

---
+++ build/precache-manifest.ee55bbafe715c1c4eaf0327eac59838b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a8ff763b9107356cc867",
+    "url": "/static/css/main.be76e5bc.chunk.css"
+  },
+  {
+    "revision": "a8ff763b9107356cc867",
+    "url": "/static/js/main.a8ff763b.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "827af7126768f3745c21",
+    "url": "/static/js/2.827af712.chunk.js"
+  },
+  {
+    "revision": "08b7c35374b1e631e29460f599477a08",
+    "url": "/index.html"
+  }
+];
